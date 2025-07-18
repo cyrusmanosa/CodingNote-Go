6cd94c2f274bc1bee8bbde0a2182b4ee8a604e3f
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2025-07-19 03:03:05 +0800

    delete go.mod and go.sum

--- 31-Database/31_2-Postgesql/Postgresql.go
+++ 31-Database/31_2-Postgesql/Postgresql.go
@@ -1,21 +1,19 @@
 package main
 
-// import (
-// 	"context"
-// 	"log"
+import (
+	"context"
+	"log"
 
-// 	"github.com/jackc/pgx/v5/pgxpool"
-// )
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
-// var DBSourceInfo = "postgresql://root:secret@localhost:5432/dating_date_info?sslmode=disable"
+var DBSourceInfo = "postgresql://root:secret@localhost:5432/dating_date_info?sslmode=disable"
 
-// func main() {
-// 	// Info
-// 	info_conn, err := pgxpool.New(context.Background(), DBSourceInfo)
-// 	if err != nil {
-// 		log.Fatal().Msg("cannot connect to Info indb")
-// 	}
-// 	defer info_conn.Close()
-// 	instore := indb.NewInfoStore(info_conn)
-
-// }
+func main() {
+	// Info
+	info_conn, err := pgxpool.New(context.Background(), DBSourceInfo)
+	if err != nil {
+		log.Fatal().Msg("cannot connect to Info indb")
+	}
+	defer info_conn.Close()
+}
